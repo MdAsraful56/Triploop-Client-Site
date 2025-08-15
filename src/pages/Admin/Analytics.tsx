e85875651a0eea195b1e15d6c03b78ed399e6f47
MdAsraful56 <work.mdasraful56@gmail.com>
2025-08-15 08:30:36 +0600

    dashboard layout create

---
+++ src/pages/Admin/Analytics.tsx
@@ -0,0 +1,10 @@
+const Analytics = () => {
+    return (
+        <div>
+            <h1>Admin Analytics</h1>
+            {/* Add your analytics UI here */}
+        </div>
+    );
+};
+
+export default Analytics;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter } from 'react-router';
 import App from '../App';
+import DashboardLayout from '../components/layout/DashboardLayout';
 import About from '../pages/About';
+import AddTour from '../pages/Admin/AddTour';
+import Analytics from '../pages/Admin/Analytics';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import Bookings from '../pages/User/Bookings';
 import Verify from '../pages/Verify';
 
 export const router = createBrowserRouter([
@@ -14,6 +18,30 @@ export const router = createBrowserRouter([
             {
                 path: 'about',
                 Component: About,
+            },
+        ],
+    },
+    {
+        path: '/admin',
+        Component: DashboardLayout,
+        children: [
+            {
+                path: 'analytics',
+                Component: Analytics,
+            },
+            {
+                path: 'add-tour',
+                Component: AddTour,
+            },
+        ],
+    },
+    {
+        path: '/user',
+        Component: DashboardLayout,
+        children: [
+            {
+                path: 'bookings',
+                Component: Bookings,
             },
         ],
     },
